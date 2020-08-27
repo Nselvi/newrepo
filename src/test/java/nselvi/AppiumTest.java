2c7372174ede83d7a59b9df68ea720e81e729bf1
nevin <nselvitopu@gmail.com>
2020-08-26 22:31:37 -0400

    first commit

---
+++ src/test/java/nselvi/AppiumTest.java
@@ -0,0 +1,18 @@
+package nselvi;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void nativeAPpAndroid() {
+		System.out.println("nativeAPpAndroid");
+
+	}
+
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+	}
+
+}
